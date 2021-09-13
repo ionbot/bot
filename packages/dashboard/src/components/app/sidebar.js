ecf3b5cba8dca020f47882b75e5d948ba7b3fa49
xencodes <xencodes@icloud.com>
2021-09-13 14:33:48 +0530

    fix(lang): translations for sidebar

--- packages/dashboard/src/components/app/sidebar.js
+++ packages/dashboard/src/components/app/sidebar.js
@@ -1,13 +1,32 @@
 import { Box, Heading, Text, VStack } from '@chakra-ui/layout'
 import { Link } from 'react-router-dom'
+import { FiHome, FiPackage, FiSettings } from 'react-icons/fi'
 
-import { SidebarItems } from '../../config/sidebar'
 import { UserStore } from '../../store/user'
+import { useTranslation } from 'react-i18next'
 
 export const AppSidebar = ({ active }) => {
+	const { t } = useTranslation()
 	const ionVersion = UserStore.useState((s) => s.ionVersion)
-	const itemKeys = Object.keys(SidebarItems)
 
+	const SidebarItems = {
+		home: {
+			name: t('common.home'),
+			icon: <FiHome />,
+			color: 'brand.300',
+		},
+		modules: {
+			name: t('common.modules'),
+			icon: <FiPackage />,
+			color: 'purple.300',
+		},
+		settings: {
+			name: t('common.settings'),
+			icon: <FiSettings />,
+			color: 'red.300',
+		},
+	}
+	const itemKeys = Object.keys(SidebarItems)
 	return (
 		<Box h='100vh' shadow='lg'>
 			<VStack userSelect='none' spacing={12} py={8}>
