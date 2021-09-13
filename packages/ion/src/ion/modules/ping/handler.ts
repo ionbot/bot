895401461bec297b6d104d5d1f40845924e12fa9
xencodes <xencodes@icloud.com>
2021-09-13 21:20:17 +0530

    chore: update ping text

--- packages/ion/src/ion/modules/ping/handler.ts
+++ packages/ion/src/ion/modules/ping/handler.ts
@@ -1,11 +1,26 @@
 import { TelegramClient } from 'telegram'
 import { NewMessageEvent } from 'telegram/events'
+const { version } = require('../../../../package.json')
 
 export const PingHandler = async (
 	client: TelegramClient,
 	event: NewMessageEvent
 ) => {
+	const time = Date.now()
+	await event.message.edit({ text: '...' })
+
+	const diff = Date.now() - time
+
+	const latency = `🕔 Latency: **${
+		diff / 2 // haha
+	}ms**\n`
+
+	let text = `🔥 Ion v${version} is up and running.\n`
+
+	text += latency
+
 	await event.message.edit({
-		text: 'Pong',
+		text,
+		parseMode: 'markdown',
 	})
 }
