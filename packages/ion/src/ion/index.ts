152c549c59989a5d00f329ca982776d7e1568066
xencodes <xencodes@icloud.com>
2021-09-13 14:07:19 +0530

    fix: add api validation

--- packages/ion/src/ion/index.ts
+++ packages/ion/src/ion/index.ts
@@ -2,6 +2,7 @@ import { TelegramClient } from 'telegram'
 import { getUserCreds } from '../utils/getUserCred'
 class Ion {
 	private client?: TelegramClient
+	isRunning: Boolean = false
 
 	constructor() {
 		// initiate Ion
@@ -10,16 +11,17 @@ class Ion {
 
 	async init() {
 		const { apiId, apiHash, session } = await getUserCreds()
-
-		this.client = new TelegramClient(session, apiId, apiHash, {
-			connectionRetries: 5,
-		})
-
-		this.client.start({ botAuthToken: '' })
+		if (!apiId || !apiHash || !session) {
+			this.client = new TelegramClient(session, apiId, apiHash, {
+				connectionRetries: 5,
+			})
+			await this.client.start({ botAuthToken: '' })
+			this.isRunning = true
+		}
 	}
 
 	async getProfile() {
-		if (!this.client) {
+		if (!this.client || !this.isRunning) {
 			throw new Error('ION_NOT_SETUP')
 		}
 		const profile = await this.client?.getMe()
