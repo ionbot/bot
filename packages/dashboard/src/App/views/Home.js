3db810a0ae29762c035705f829eec43606b203ce
xencodes <xencodes@icloud.com>
2021-09-13 16:03:57 +0530

    style: update animations

--- packages/dashboard/src/App/views/Home.js
+++ packages/dashboard/src/App/views/Home.js
@@ -4,10 +4,10 @@ import { motion } from 'framer-motion'
 const Home = () => {
 	return (
 		<motion.div
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
-			transition={{ duration: 0.5 }}
+			initial={{ opacity: 0, y: -20 }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0, y: -20 }}
+			transition={{ duration: 0.3 }}
 		>
 			<Box>
 				<Heading>Home</Heading>
