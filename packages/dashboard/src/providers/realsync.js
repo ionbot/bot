9498065a419213450d8827ea1059656cf6588993
xencodes <xencodes@icloud.com>
2021-09-13 22:45:13 +0530

    fix: change pathname -> host

--- packages/dashboard/src/providers/realsync.js
+++ packages/dashboard/src/providers/realsync.js
@@ -5,7 +5,7 @@ const { NODE_ENV } = process.env
 const HOST =
 	NODE_ENV === 'development'
 		? process.env.REACT_APP_HOST || 'http://10.0.0.18:4337'
-		: window.location.hostname
+		: window.location.host
 
 const realsync = new RealSync(HOST)
 export default realsync
