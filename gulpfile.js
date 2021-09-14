8b6fecc0dc5a08c9b84ecfd2491b5893c0e43d06
xencodes <xencodes@icloud.com>
2021-09-14 10:05:07 +0530

    build: improve build functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 const gulp = require('gulp')
 const run = require('gulp-run')
 
-gulp.task('build', (cb) => {
+function buildClient(cb) {
 	run('yarn build:client').exec('', () => {
-		run('yarn build:server').exec('', () => {
-			cb()
-		})
+		cb()
 	})
-})
+}
 
-gulp.task('dist', (cb) => {
+function buildServer(cb) {
+	run('yarn build:server').exec('', () => {
+		cb()
+	})
+}
+
+function build(cb) {
+	gulp.parallel(buildClient, buildServer)
+	cb()
+}
+
+function dist(cb) {
 	gulp.src('packages/dashboard/build').pipe(gulp.dest('packages/ion/'))
 	cb()
-})
+}
 
-exports.default = ['dist']
+exports.default = gulp.series(build, dist)
